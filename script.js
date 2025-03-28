e1f193be16795386755ab9b7dbb29866503bcfae
Peter Szots <103681468+PetsS@users.noreply.github.com>
2025-03-28 16:55:09 +0100

    create logic for operator signs and for calculation by pressing the equals button

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let operandA = 0;
-let operandB = 0;
-let operator = "";
-let displayValue = 0;
-
 function operate(numA, numB, operator) {
   numA = parseFloat(numA);
   numB = parseFloat(numB);
@@ -34,15 +29,67 @@ const container = document.querySelector("#container");
 const display = document.querySelector("#display");
 const buttons = container.querySelectorAll("button");
 
-buttons.forEach(button => {
+let displayValue = "";
+let temporaryNumber = 0;
+let operandA = 0;
+let operandB = 0;
+let operator = "";
+
+buttons.forEach((button) => {
   if (button.className === "operand") {
     button.addEventListener("click", () => {
+      if (temporaryNumber == 0) {
+        displayValue = 0;
+      }
+
+      temporaryNumber += button.value;
+
+      if (operandA != 0) {
+        operandB = parseFloat(temporaryNumber);
+      }
+
       displayValue += button.value;
+      display.textContent = parseFloat(displayValue);
+
+      console.log("tempNum: " + temporaryNumber);
+    });
+  } else if (button.className === "operator") {
+    button.addEventListener("click", () => {
+      if (operandA === 0) {
+        operandA = parseFloat(temporaryNumber);
+        console.log("opA: " + operandA);
+        temporaryNumber = 0;
+        console.log("tempNum: " + temporaryNumber);
+      } else if (operandB === 0) {
+        operandB = parseFloat(temporaryNumber);
+        console.log("opB: " + operandB);
+        temporaryNumber = 0;
+        console.log("tempNum: " + temporaryNumber);
+      } else {
+        operandA = operate(operandA, operandB, operator);
+        operandB = 0;
+        console.log("opA: " + operandA);
+        temporaryNumber = 0;
+        console.log("tempNum: " + temporaryNumber);
+      }
+      operator = button.value;
+      console.log("operator: " + operator);
+
+      displayValue = parseFloat(operandA);
       display.textContent = displayValue;
     });
-  };
+  } else if (button.className === "equals") {
+    button.addEventListener("click", () => {
+      if (operandA !== 0 || operandB !== 0) {
+        displayValue = operate(operandA, operandB, operator);
+        temporaryNumber = 0;
+      } else {
+        displayValue = parseFloat(temporaryNumber);
+      }
+
+      display.textContent = displayValue;
+      console.log("opA: " + operandA);
+      console.log("opB: " + operandB);
+    });
+  }
 });
-
-
-
-console.log(operate("3", "2", "-"));
